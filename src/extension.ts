a1e45eac75c7e8fad56f628b465706e279154cfc
Michael Bowen <mikee.bowen@gmail.com>
2021-03-22 10:01:52 -0700

    clean up extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, ExtensionContext } from 'vscode';
 import OOXMLValidator from './ooxm-validator';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "ooxml-validator-vscode" is now active!');
+export function activate(context: ExtensionContext) {
+  const validateOOXML = commands.registerCommand('ooxml-validator-vscode.validateOOXML', file => OOXMLValidator.validate(file));
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('ooxml-validator-vscode.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from ooxml-validator-vscode!');
-  });
-  const validateOOXML = vscode.commands.registerCommand('ooxml-validator-vscode.validateOOXML', file => OOXMLValidator.validate(file));
-
-  context.subscriptions.push(disposable, validateOOXML);
+  context.subscriptions.push(validateOOXML);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
