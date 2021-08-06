347b6e7835de735466b81efc329430f43a2eebf6
Michael Bowen <mikee.bowen@gmail.com>
2021-08-06 09:22:39 -0700

    add gulp and got to rebuild electron

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 'use strict';
 const { rebuild } = require('electron-rebuild');
+const { exec } = require('child_process');
+const got = require('got');
+const { promisify } = require('util');
+const execPromise = promisify(exec);
 
 async function defaultTask(cb) {
-  await rebuild({
-    buildPath: __dirname,
-    electronVersion: '13.1.7',
-  });
-  cb();
+  try {
+    const { stdout: version } = await execPromise('code --version');
+    const data = await got(`https://raw.githubusercontent.com/microsoft/vscode/${version.split('\n')[0]}/.yarnrc`);
+    const electronVersion = data.body.split('\n')[1].split(' ')[1].replace(/"/g, '');
+
+    await rebuild({
+      buildPath: __dirname,
+      electronVersion,
+    });
+    cb();
+  } catch (error) {
+    console.error(error.response.body);
+  }
 }
 
 exports.default = defaultTask;
